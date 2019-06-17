7df37631197c30e938d99bfb01395bcb622c651d
Arata Tanaka <tarata43@yahoo.co.jp>
2019-06-17 23:42:33 +0900

    Fix test script

--- test/fujiwara.js
+++ test/fujiwara.js
@@ -2,15 +2,22 @@ const assert = require('power-assert');
 const Helper = require('hubot-test-helper');
 const helper = new Helper('../src/scripts');
 
-describe('fujiwara', async () => {
-    room = helper.createRoom();
-    await room.user.say('user', 'hubot fujiwara あああ');
-    it('fujiwara あああ', () => {
+describe('fujiwara', () => {
+    let room = null;
+
+    beforeEach(() => {
+        room = helper.createRoom();
+    })
+
+    afterEach(() => {
+        room.destroy();
+    });
+
+    it('fujiwara あああ', async () => {
+        await room.user.say("user", "hubot fujiwara あああ");
         assert.deepEqual(room.messages, [
             ['user', 'hubot fujiwara あああ'],
             ['hubot', 'あ゛あ゛あ゛']
         ]);
     });
-
-    room.destroy();
 });
